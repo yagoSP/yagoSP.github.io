d1d238c71b318fa164e37e49d8cd1159b49c89eb
Yago <54825415+yagoSP@users.noreply.github.com>
2022-01-01 22:50:52 -0300

    Update calc.js

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,4 +1,4 @@
 var n1 = Number(window.prompt('Digite um número:'));
-var n2 = Number(Window.prompt('Digite outro número:'));
+var n2 = Number(window.prompt('Digite outro número:'));
 var result =  n1 + n2;
 window.alert(`${n1} + ${n2} = ${result}`);
